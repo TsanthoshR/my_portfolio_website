283c6e50ffc8f8854ef93453c3c0b274d3ca318a
TSanthosh <santhosht5290@gmail.com>
2021-08-08 01:40:21 +0530

    remove ignored files

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=100)
     date = models.DateField()
+
+
+    def __str__(self):
+        return self.title
