6a75f159d74f730f9e1921bb2bca1444913e5985
TSanthosh <santhosht5290@gmail.com>
2021-08-08 00:14:40 +0530

    pluralize blog posts home page

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 # Create your views here.
 def all_blogs(request):
-    Blog_objects = Blog.objects.order_by('-date')[:1]
+    Blog_objects = Blog.objects.order_by('-date') #[:2]
 
     return render(request, 'blog/all_blogs.html',{'Blogs':Blog_objects})
+
+def details(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    dict_to_pass={'blog_id':blog_id,
+                  'blog':blog,
+                    }
+    return render(request, 'blog/details.html',dict_to_pass)
