6a4e3bd92390b49094f0e9b60e77c376e9b9ad14
TSanthosh <santhosht5290@gmail.com>
2021-08-07 20:35:06 +0530

    added static image and pdf

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
 def all_blogs(request):
-    return render(request, 'blog/all_blogs.html')
+    Blog_objects = Blog.objects.order_by('-date')[:1]
+
+    return render(request, 'blog/all_blogs.html',{'Blogs':Blog_objects})
